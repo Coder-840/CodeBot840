89a4b253afc9b8a060928178fafaa5adea59c139
Coder-840 <rhcoding22@gmail.com>
2026-02-21 19:40:52 -0500

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const botArgs = {
 const PASSWORD = 'YourSecurePassword123';
 let chatLogs = [];
 let ignoreMode = true;
-const ignoreAllowed = new Set(['player_840', 'chickentender','IG_T3V_2K']);
+const ignoreAllowed = new Set(['player_840', 'chickentender','ig_t3v_2k']);
 let hunting = false;
 
 // ===== 3 MUSKETEERS SYSTEM =====
